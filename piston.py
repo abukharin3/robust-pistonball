2ffb8b7159690db4f5b129e05f95d8c20386baf1
Alexander Bukharin <abukharin@lawn-128-61-28-101.lawn.gatech.edu>
2022-08-09 16:02:49 -0400

    Change ball mass

--- piston.py
+++ piston.py
@@ -42,32 +42,35 @@ def train(model_path='models/policy', exp_name=''):
     model.save(model_path)
     np.save("results/train_{}.npy".format(exp_name), np.array(rewards))
 
-def eval(model_path='models/policy', exp_name=''):
-    env = pistonball_v6.env(
-        n_pistons=20,
-        time_penalty=-0.1,
-        continuous=True,
-        random_drop=True,
-        random_rotate=True,
-        ball_mass=0.75,
-        ball_friction=0.3,
-        ball_elasticity=1.5,
-        max_cycles=125,
-    )
-    env = ss.color_reduction_v0(env, mode="B")
-    env = ss.resize_v1(env, x_size=84, y_size=84)
-    env = ss.frame_stack_v1(env, 3)
-
-    model = PPO.load(model_path)
-
-    env.reset()
+def eval(model_path='models/policy', exp_name='', ball_mass=0.75, ball_friction=0.3):
     rewards = []
-    for agent in env.agent_iter():
-        obs, reward, done, info = env.last()
-        rewards.append(reward)
-        act = model.predict(obs, deterministic=True)[0] if not done else None
-        env.step(act)
-        # env.render()
+    for ball_mass in [0.1, 0.3, 0.6, 0.75, 1.0, 1.5]:
+        env = pistonball_v6.env(
+            n_pistons=20,
+            time_penalty=-0.1,
+            continuous=True,
+            random_drop=True,
+            random_rotate=True,
+            ball_mass=ball_mass,
+            ball_friction=0.3,
+            ball_elasticity=0.1,
+            max_cycles=125,
+        )
+        env = ss.color_reduction_v0(env, mode="B")
+        env = ss.resize_v1(env, x_size=84, y_size=84)
+        env = ss.frame_stack_v1(env, 3)
+
+        model = PPO.load(model_path)
+
+        env.reset()
+        envrewards = []
+        for agent in env.agent_iter():
+            obs, reward, done, info = env.last()
+            venrewards.append(reward)
+            act = model.predict(obs, deterministic=True)[0] if not done else None
+            env.step(act)
+            #env.render()
+        rewards.append(envrewards)
     np.save("results/eval_{}.npy".format(exp_name), np.array(rewards))
 
 if __name__ == "__main__":
